9e99029804c0773b706e83925e10c702286ddcd1
Pauline Neuville <pauline.neuville@gmail.com>
2023-07-03 10:26:18 +0200

    commit

--- js/character.js
+++ js/character.js
@@ -1,15 +1,28 @@
 const characterId = (new URLSearchParams(window.location.search)).get('id');
-fetch('https://character-database.becode.xyz/characters/' + id,init)
-.then((response) => response.json())
-.then(character =>{
+fetch('https://character-database.becode.xyz/characters/' + characterId)
+    .then((response) => response.json())
+    .then(character =>{
+
+    let div = document.createElement("div");
+    div.className = "character";
+    
+    let image = new Image();
+    image.src = 'data:image/png;base64,'+ character["image"];
+    div.appendChild(image);
+    image.style.width ="100%";
+
+
+    let nameBlock = document.getElementById("p"); // amener le nom du character
+    nameBlock.innerText = character["name"]; // dire que y aura du texte et que c nom de l'api
+    nameBlock.className = "nameBlock";  // mettre une classe nameBlock
+    div.appendChild(nameBlock);
 
 
 
 
-    let nameBlock = document.getElementById("p"); // 2.4.
-    nameBlock.innerText = character["name"]; //2.5.
-    nameBlock.className = "nameBlock"; 
 
 })
 .catch(error=> {
+
+
 });
